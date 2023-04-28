c5fd93fc3ac16817d4dfb092da374cf41d742afd
cherono29 <vickykosgei332@gmail.com>
2023-04-28 14:50:03 +0300

    first commit

---
+++ hospital/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import (Slider, Service, Item, Doctor, Expertize, Faq, Gallery)
+
+admin.site.register(Slider)
+admin.site.register(Service)
+admin.site.register(Doctor)
+admin.site.register(Expertize)
+admin.site.register(Faq)
+admin.site.register(Gallery)
